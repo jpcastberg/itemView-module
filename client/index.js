272f29d9752fd9a9143cf35f0bcb266f2336ab88
John Castberg <jpcastberg@gmail.com>
2019-02-06 20:08:53 -0800

    init

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>React is mounted!!</div>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
