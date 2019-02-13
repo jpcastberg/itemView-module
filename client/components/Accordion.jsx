d2ae5efeaace6fbe4e9986328fdfa0e9e81cedfe
John Castberg <jpcastberg@gmail.com>
2019-02-13 15:57:26 -0800

    Implement basic accordion logic

--- client/components/Accordion.jsx
+++ client/components/Accordion.jsx
@@ -7,14 +7,37 @@ export default class Accordion extends Component {
     this.state = {
       isDisplayed: false,
     };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+
+  generateClassName() {
+    let className = '';
+    const { isDisplayed } = this.state;
+    if (isDisplayed) {
+      className = 'accordian-content-displayed';
+    } else {
+      className = 'accordian-content-hidden';
+    }
+    return className;
+  }
+
+  toggleDisplay() {
+    const { isDisplayed } = this.state;
+    this.setState({ isDisplayed: !isDisplayed });
   }
 
   render() {
     return (
-      <div>
-        This is a test!!!!
+      <div onClick={() => {
+        this.toggleDisplay();
+      }}>
+        CONTAINER
+        <div className={this.generateClassName()}>
+          This is a test!!!!
+        </div>
+        Test content
       </div>
-    )
+    );
   }
 }
 
