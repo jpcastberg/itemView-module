b40c99d7349ffdcd1e7ea0cc70b12723e76e86c5
John Castberg <jpcastberg@gmail.com>
2019-02-17 11:48:19 -0800

    Complete currentPagePath component and styling

--- client/components/currentPagePath.jsx
+++ client/components/currentPagePath.jsx
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types';
 
 export default function CurrentPagePath(props) {
   const { breadcrumbs } = props;
+
   const breadcrumbEles = breadcrumbs.map((breadcrumb, idx) => {
     const breadcrumbDivider = idx < breadcrumbs.length - 1
       ? (<span className="product-meta breadcrumb-divider">/</span>)
       : null;
+    const breadcrumbClassName = idx < breadcrumbs.length - 1
+      ? 'product-meta breadcrumb'
+      : 'product-meta breadcrumb-item-name';
     return (
       <span>
-        <span className="product-meta breadcrumb">{breadcrumb}</span>
+        <span className={breadcrumbClassName}>{breadcrumb}</span>
         { breadcrumbDivider }
       </span>
     );
