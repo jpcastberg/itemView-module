6b02aaff67aed6db9504190202a8f52b1fb867cc
John Castberg <jpcastberg@gmail.com>
2019-02-18 17:22:00 -0800

    Update webpack modules and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'env'],
+          presets: ['@babel/preset-react', '@babel/preset-env'],
         },
       },
       {
