c3ce300f9cb29a0b5d4fe33771deba6fb304f7a3
John Castberg <jpcastberg@gmail.com>
2019-02-19 14:26:18 -0800

    Enable server to handle all cors requests

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 const { dbConnection, items } = require('../database/index.js');
 
 const app = express();
 const port = 3002;
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
