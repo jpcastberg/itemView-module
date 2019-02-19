dfc87b76649ff224ac065e7607642f174a8a17d8
John Castberg <jpcastberg@gmail.com>
2019-02-19 11:29:27 -0800

    Use 'path' in webpack config context

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  context: __dirname + '/client',
+  context: path.join(__dirname, '/client'),
   entry: './index.jsx',
   module: {
     rules: [
